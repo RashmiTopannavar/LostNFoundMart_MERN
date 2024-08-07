c7d7c65aab67137186b2db08794e71d8d52adb4b
Rashmi Venkatesh Topannavar <rashmi.topannavar@gmail.com>
2024-08-07 01:58:31 -0500

    React routes implemented

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,45 +1,38 @@
-import { Navbar, Nav, Container } from 'react-bootstrap'; // Importing necessary components from react-bootstrap
-import { FaShoppingCart, FaUser } from 'react-icons/fa'; //(Font Awesome set)
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import { FaShoppingCart, FaUser } from 'react-icons/fa';
+import { LinkContainer } from 'react-router-bootstrap'
 import logo from '../assets/logo.png';
 
-
-// Define the Header functional component
 const Header = () => {
     return (
-        // Semantic HTML5 header element
         <header>
-            {/* Navbar component from react-bootstrap with dark theme, expands on large screens, and collapses on select */}
-            <Navbar bg='dark' variant='dark' expand='md' collapseOnSelect>
-                {/* Container component to center content */}
+            <Navbar bg = 'dark' variant = 'dark' expand = 'md' collapseOnSelect>
                 <Container>
-                    {/* Navbar brand component with a link to the home page */}
-                    <Navbar.Brand href='/'>
-                    <img src={logo} alt='LostNFoundMart'/>
-                    LostNFoundMart
-                    </Navbar.Brand>
-                    {/* Navbar toggle button for responsive navigation */}
+                    <LinkContainer to = '/'>
+                        <Navbar.Brand>
+                            <img src = {logo} alt = 'LostNFount Mart' />
+                            Lost N Found Mart
+                        </Navbar.Brand>
+                    </LinkContainer>    
                     <Navbar.Toggle aria-controls='basic-navbar-nav' />
-                    {/* Collapsible part of the navbar, contains the navigation links */}
-                    <Navbar.Collapse id='basic-navbar-nav'>
-                        {/* Nav component to contain navigation links, aligned to the right with ms-auto class */}
+                    <Navbar.Collapse id = 'basic-navbar-nav'>
                         <Nav className='ms-auto'>
-                            {/* Navigation link to the cart page */}
-                            <Nav.Link href='/cart'>
-                                {/* Shopping cart icon followed by "Cart" text */}
-                                <FaShoppingCart /> Cart
-                            </Nav.Link>
-                            {/* Navigation link to the login page */}
-                            <Nav.Link href='/login'>
-                                {/* User icon followed by "Sign In" text */}
-                                <FaUser /> Sign In
-                            </Nav.Link>
+                            <LinkContainer to= '/cart'>
+                                <Nav.Link >
+                                    <FaShoppingCart /> Cart
+                                </Nav.Link>
+                            </LinkContainer>
+                            <LinkContainer to = '/Login'>
+                                <Nav.Link>
+                                    <FaUser /> Sign In
+                                </Nav.Link>
+                            </LinkContainer>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
             </Navbar>
         </header>
-    );
+    )
 }
 
-// Export the Header component as the default export
 export default Header;
