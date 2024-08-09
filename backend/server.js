13c1f13b751d1e42cf174c88713c5fcbc1d4e5c0
Rashmi Venkatesh Topannavar <rashmi.topannavar@gmail.com>
2024-08-08 19:41:52 -0500

    environment variables implemented

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
 import express from 'express'
+import dotenv from 'dotenv';
+dotenv.config();
 import products from './data/products.js';
+const port = process.env.PORT || 8080;
 
-const port = 8000;
+
 
 const app = express();
 
