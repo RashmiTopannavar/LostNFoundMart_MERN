03db4d1133b30323937f53ad82f5c9260b733372
Rashmi Venkatesh Topannavar <rashmi.topannavar@gmail.com>
2024-08-09 17:10:25 -0500

    Loader and message component implemented

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -3,6 +3,8 @@ import { Link } from "react-router-dom";
 import {Row, Col, Image, ListGroup, Card, Button} from 'react-bootstrap';
 import Ratings from "../components/Ratings";
 import { useGetProductDetailsQuery } from "../slices/productsApiSlice";
+import Loader from "../components/Loader";
+import Message from "../components/Message";
 
 
 const ProductScreen = () =>{
@@ -16,10 +18,11 @@ const ProductScreen = () =>{
         <Link className="btn btn-light my-3" to='/'>
              Back
         </Link>
+
         {isLoading ? (
-            <h2>Loading...</h2>
+            <Loader/>
         ) : error ? (
-        <div> {error?.data?.message || error.error }</div>
+        <Message variant='danger'>{error?.data?.message || error.error }</Message>
     ): (
             <>
             <Row>
