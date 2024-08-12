ea504303eb24ca16d0934e6e4882ca6db761dd1d
Rashmi Venkatesh Topannavar <rashmi.topannavar@gmail.com>
2024-08-12 01:25:32 -0500

    User email and password implemented

--- backend/server.js
+++ backend/server.js
@@ -7,10 +7,15 @@ const port = process.env.PORT || 8080;
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+import exp from 'constants';
 
 connectDB(); //Connect to MongoDB
 
 const app = express();
+
+//Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
     res.send('API is running...')
