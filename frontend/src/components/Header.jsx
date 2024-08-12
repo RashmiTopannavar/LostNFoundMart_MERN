df1911188e54ecd7ff9b0311255deed99d5a230e
Rashmi Venkatesh Topannavar <rashmi.topannavar@gmail.com>
2024-08-11 19:07:39 -0500

    Cart number in header implemented

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,9 +1,14 @@
-import { Navbar, Nav, Container } from 'react-bootstrap';
+import { Navbar, Nav, Container, Badge } from 'react-bootstrap';
 import { FaShoppingCart, FaUser } from 'react-icons/fa';
 import { LinkContainer } from 'react-router-bootstrap'
 import logo from '../assets/logo.png';
+import {useSelector} from 'react-redux';
 
 const Header = () => {
+
+    const { cartItems } = useSelector((state) => state.cart);
+    console.log(cartItems);
+
     return (
         <header>
             <Navbar bg = 'dark' variant = 'dark' expand = 'md' collapseOnSelect>
@@ -20,6 +25,22 @@ const Header = () => {
                             <LinkContainer to= '/cart'>
                                 <Nav.Link >
                                     <FaShoppingCart /> Cart
+
+                                    {
+                                cartItems.length > 0 && (
+                                    //Bootstrap component used to display a badge, which is typically a small piece of information, such as a count or status.
+                                    //Pill-look like a pill shape
+                                    <Badge pill bg='success' style={{marginLeft: '5px'}}>  
+                                        {cartItems.reduce((a, c) => a + c.qty, 0)}  
+                                        {/* /*a: Accumulator, which holds the running total.
+                                                                                    c: Current item in the array being processed.
+                                                                                    c.qty: The quantity of the current item.
+                                                                                    a + c.qty: Adds the current item's quantity to the accumulator.
+                                                                                    0: The initial value of the accumulator.*/}
+                                    </Badge>
+                                )
+                            }
+
                                 </Nav.Link>
                             </LinkContainer>
                             <LinkContainer to = '/Login'>
---
+++ frontend/src/screens/CartScreen.jsx
@@ -0,0 +1,9 @@
+import { } from 'react-router-dom'
+
+const CartScreen = () =>{
+    return (
+        <div>Cart</div>
+    )
+}
+
+export default CartScreen;
