8d6651dc95f33db4ae97f26fc7294e2c3a0a4295
Rashmi Venkatesh Topannavar <rashmi.topannavar@gmail.com>
2024-08-12 01:02:25 -0500

    User Controller and Router implemented

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import connectDB from './config/db.js';
 import products from './data/products.js';
 const port = process.env.PORT || 8080;
 import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 connectDB(); //Connect to MongoDB
@@ -16,6 +17,7 @@ app.get('/', (req, res) => {
 })
 
 app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
