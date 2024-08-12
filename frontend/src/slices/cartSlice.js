47c07dd13f00757fd319ffb41f95d99458170e04
Rashmi Venkatesh Topannavar <rashmi.topannavar@gmail.com>
2024-08-11 22:15:09 -0500

    Remove from cart implemented

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -25,8 +25,12 @@ const cartSlice = createSlice({
             }
 
             return updateCart(state);
+        },
+        removeFromCart: (state, action) =>{
+            state.cartItems = state.cartItems.filter((x) => x._id !== action.payload) //retains all elements in UI, which r not removed in the order
+            return updateCart(state);
         }
     },
 })
-export const { addToCart } = cartSlice.actions;
+export const { addToCart, removeFromCart } = cartSlice.actions;
 export default cartSlice.reducer;
