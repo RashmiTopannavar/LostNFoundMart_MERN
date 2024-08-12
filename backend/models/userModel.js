956f3bcad72746ec05af665de03cb99a649dc6e3
Rashmi Venkatesh Topannavar <rashmi.topannavar@gmail.com>
2024-08-12 12:07:00 -0500

    User Register endpoint and encryption implemented

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -31,6 +31,21 @@ userSchema.methods.matchPassword = async function (enteredPassword){
   return await bcrypt.compare(enteredPassword, this.password);
 }
 
+userSchema.pre('save', async function (next) {  //runs before password is saved to the database.
+  
+  // Check if the password field has not been modified
+  if (!this.isModified('password')) {
+    next(); // If not modified, skip the hashing process and proceed to the next middleware
+  }
+
+  // Generate a salt with a cost factor of 10
+  const salt = await bcrypt.genSalt(10);
+
+  // Hash the password using the generated salt and assign it to the user's password field
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+
 const User = mongoose.model('User', userSchema);
 
 export default User;
