6b8fd5ccfc7220ad13e313ee2cce14b875fd21ce
Rashmi Venkatesh Topannavar <rashmi.topannavar@gmail.com>
2024-08-13 02:38:40 -0500

    Checkout steps implemented

--- frontend/src/screens/ShippingScreen.jsx
+++ frontend/src/screens/ShippingScreen.jsx
@@ -4,6 +4,7 @@ import FormContainer from '../components/FormContainer';
 import { useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { saveShippingAddress } from "../slices/cartSlice";
+import CheckoutSteps from "../components/CheckoutSteps";
 
 
 const ShippingScreen =() =>{
@@ -32,7 +33,9 @@ const ShippingScreen =() =>{
     }
     return(
         <FormContainer>
+            <CheckoutSteps step1 step2/>
       <h1>Shipping</h1>
+
       <Form onSubmit={submitHandler}>
         <Form.Group className='my-2' controlId='address'>
           <Form.Label>Address</Form.Label>
