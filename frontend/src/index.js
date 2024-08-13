541515e0e403108dc786a4f0f09861fda0f4ebcd
Rashmi Venkatesh Topannavar <rashmi.topannavar@gmail.com>
2024-08-13 02:01:10 -0500

    Private Route implemented

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -20,6 +20,7 @@ import CartScreen from './screens/CartScreen';
 import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
 import ShippingScreen from './screens/ShippingScreen';
+import PrivateRoute from './components/PrivateRoute';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -30,6 +31,10 @@ const router = createBrowserRouter(
       <Route path='/login' element={<LoginScreen/>}/> 
       <Route path = '/register' element={<RegisterScreen/>}/>
       <Route path = '/shipping' element={<ShippingScreen/>}/>
+
+      <Route path='' element={<PrivateRoute/>}>
+      <Route path = '/shipping' element={<ShippingScreen/>}/>
+      </Route>
     </Route>
   )
 )
