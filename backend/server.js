d46336293d8ac46ec4973082f72a850ac89ffa08
Rashmi Venkatesh Topannavar <rashmi.topannavar@gmail.com>
2024-08-13 12:39:05 -0500

    Order controller & Router  implemented

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 8080;
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
-import exp from 'constants';
+import orderRoutes from './routes/orderRoutes.js';
 
 connectDB(); //Connect to MongoDB
 
@@ -27,6 +27,7 @@ app.get('/', (req, res) => {
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
+app.use('/api/orders', orderRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
