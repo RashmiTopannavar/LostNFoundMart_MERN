87fa31641d824a63e59d10bbc5dde6bf91078d3f
Rashmi Venkatesh Topannavar <rashmi.topannavar@gmail.com>
2024-08-15 13:09:59 -0500

    List orders for Admin implemented

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -25,6 +25,8 @@ import PaymentScreen from './screens/PaymentScreen';
 import PlaceOrderScreen from './screens/PlaceOrderScreen';
 import OrderScreen from './screens/OrderScreen';
 import ProfileScreen from './screens/ProfileScreen';
+import OrderListScreen from './screens/admin/OrderListScreen';
+import AdminRoute from './components/AdminRoute';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -42,6 +44,11 @@ const router = createBrowserRouter(
         <Route path= '/order/:id' element={<OrderScreen/>}/>
         <Route path='/profile' element={<ProfileScreen/>}/>
       </Route>
+
+      <Route path = '' element={<AdminRoute/>}>
+        <Route path='/admin/orderlist' element={<OrderListScreen/>}/>
+      </Route>
+
     </Route>
   )
 )
