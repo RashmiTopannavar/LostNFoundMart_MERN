a893901a77ce4b305041cb013de3dea0503ae858
Rashmi Venkatesh Topannavar <rashmi.topannavar@gmail.com>
2024-08-16 17:34:51 -0500

    Customer reviews

--- frontend/src/slices/productsApiSlice.js
+++ frontend/src/slices/productsApiSlice.js
@@ -52,6 +52,14 @@ export const productsApiSlice = apiSlice.injectEndpoints({
                 method: 'DELETE',
             }),
         }),
+        createReview: builder.mutation({
+            query: (data) => ({
+                url: `${PRODUCTS_URL}/${data.productId}/reviews`,
+                method: 'POST',
+                body: data,
+            }),
+            invalidatesTags: ['Product'],
+        })
 
     }),
 });
@@ -64,4 +72,5 @@ export const {
     useUpdateProductMutation, 
     useUploadProductImageMutation,
     useDeleteProductMutation,
+    useCreateReviewMutation,
 } = productsApiSlice;
