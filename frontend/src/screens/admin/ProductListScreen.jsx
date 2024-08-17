d2ff9b333b4cbbd69f391f826e12fa8643c30a96
Rashmi Venkatesh Topannavar <rashmi.topannavar@gmail.com>
2024-08-16 20:05:49 -0500

    Paginate Component

--- frontend/src/screens/admin/ProductListScreen.jsx
+++ frontend/src/screens/admin/ProductListScreen.jsx
@@ -8,11 +8,17 @@ import {
   useCreateProductMutation,
 useDeleteProductMutation } from '../../slices/productsApiSlice';
 import { toast } from 'react-toastify';
+import { useParams } from 'react-router-dom';
+import Paginate from '../../components/Paginate';
 
 
 const ProductListScreen = () => {
 
-    const {data: products, isLoading, error, refetch } = useGetProductsQuery();
+  const { pageNumber } = useParams();
+
+  const { data, isLoading, error, refetch } = useGetProductsQuery({
+    pageNumber,
+  });
 
     const createProductHandler = async () => {
       if(window.confirm('Are you sure you want to create a new product?')){
@@ -76,7 +82,7 @@ const ProductListScreen = () => {
                   </tr>
                 </thead>
                 <tbody>
-                  {products.map((product) => (
+                  {data.products.map((product) => (
                     <tr key={product._id}>
                       <td>{product._id}</td>
                       <td>{product.name}</td>
@@ -102,6 +108,7 @@ const ProductListScreen = () => {
                 </tbody>
               </Table>
               {/* PAGINATE PLACEHOLDER */}
+              <Paginate pages={data.pages} page={data.page} isAdmin={true} />
             </>
         )}
         </>
