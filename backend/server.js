6cc34e07a9c34844323879810aae9f79c03f3b57
Rashmi Venkatesh Topannavar <rashmi.topannavar@gmail.com>
2024-08-17 02:58:19 -0500

    Prepare for production

--- backend/server.js
+++ backend/server.js
@@ -12,34 +12,49 @@ import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import orderRoutes from './routes/orderRoutes.js';
 import uploadRoutes from './routes/uploadRoutes.js';
 
-connectDB(); //Connect to MongoDB
+connectDB(); // Connect to Database MongoDB
 
 const app = express();
 
-//Body parser middleware
+//Body Parser Middleware
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ ectended:true }))
 
-//Cookie parser middleware
+// cookie Parser Middlewate
 app.use(cookieParser());
 
-app.get('/', (req, res) => {
-    res.send('API is running...')
-})
+app.use('/api/products',productRoutes);
+app.use('/api/users',userRoutes);
+app.use('/api/orders',orderRoutes);
+app.use('/api/upload', uploadRoutes);
 
-app.use('/api/products', productRoutes);
-app.use('/api/users', userRoutes);
-app.use('/api/orders', orderRoutes);
-app.use('/api/upload',uploadRoutes);
+app.get('/api/config/paypal', (req, res) =>
+    res.send({ clientId: process.env.PAYPAL_CLIENT_ID })
+  );
 
-app.get('/api/config/paypal', (req, res) => 
-    res.send({ clientId : process.env.PAYPAL_CLIENT_ID })
-);
+const __dirname = path.resolve();  // set __dirname to current Directory
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+  
+if (process.env.NODE_ENV === 'production') {
+  const __dirname = path.resolve();
+  app.use('/uploads', express.static('/var/data/uploads'));
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
 
-const __dirname = path.resolve();
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  );
+} else {
+  const __dirname = path.resolve();
   app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+  app.get('/', (req, res) => {
+    res.send('API is running....');
+  });
+}
+
 
 app.use(notFound);
 app.use(errorHandler);
 
-app.listen(port, () => console.log(`Server is running at port ${port}`));
+app.listen(port, () =>
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`)
+);
